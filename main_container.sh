28500096ae12358073a5e7d31e1fcb2f47be356b
millerm <marshalldavidmiller@gmail.com>
2021-11-30 20:51:26 -0800

    Fix formatting

--- main_container.sh
+++ main_container.sh
@@ -9,9 +9,9 @@ echo "Launching Main Container: $@"
 $MAIN_CONTAINER_CMD
 
 notify_temination() {
-	[ ! -d $TMP_POD_DIR ] && mkdir -p $TMP_POD_DIR
-
-	touch $TERMINATED_FILE
+    [ ! -d $TMP_POD_DIR ] && mkdir -p $TMP_POD_DIR
+    
+    touch $TERMINATED_FILE
 }
 
 trap notify_temination EXIT
--- side_car_container.sh
+++ side_car_container.sh
@@ -4,26 +4,26 @@ SIDE_CAR_CMD=$@
 TERMINATED_FILE="/tmp/pod/main-terminated"
 
 launch_main_container() {
-  echo "Launching Side Car Container: $@"
-  $SIDE_CAR_CMD
+    echo "Launching Side Car Container: $@"
+    $SIDE_CAR_CMD
 }
 
 watch_for_termination() {
-  while true; do
-    if [[ -f $TERMINATED_FILE ]]; then
-      kill $SIDE_CAR_PID
-    fi
-
-    sleep 5
-  done
+    while true; do
+        if [[ -f $TERMINATED_FILE ]]; then
+            kill $SIDE_CAR_PID
+        fi
+        
+        sleep 5
+    done
 }
 
 cleanup() {
-  wait $SIDE_CAR_PID &&
+    wait $SIDE_CAR_PID &&
     if [[ -f $TERMINATED_FILE ]]; then
-      exit 0
+        exit 0
     fi
-  }
+}
 
 launch_main_container &
 SIDE_CAR_PID=$! && watch_for_termination &
