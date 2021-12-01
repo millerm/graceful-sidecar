366b3af75b3c21f8448e2575b3e332b515add669
millerm <marshalldavidmiller@gmail.com>
2021-11-30 20:50:07 -0800

    Initial commit

---
+++ main_container.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+MAIN_CONTAINER_CMD=$@
+TMP_POD_DIR="/tmp/pod"
+TERMINATED_FILE="/tmp/pod/main-terminated"
+
+echo "Launching Main Container: $@"
+
+$MAIN_CONTAINER_CMD
+
+notify_temination() {
+	[ ! -d $TMP_POD_DIR ] && mkdir -p $TMP_POD_DIR
+
+	touch $TERMINATED_FILE
+}
+
+trap notify_temination EXIT
+
---
+++ side_car_container.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+SIDE_CAR_CMD=$@
+TERMINATED_FILE="/tmp/pod/main-terminated"
+
+launch_main_container() {
+  echo "Launching Side Car Container: $@"
+  $SIDE_CAR_CMD
+}
+
+watch_for_termination() {
+  while true; do
+    if [[ -f $TERMINATED_FILE ]]; then
+      kill $SIDE_CAR_PID
+    fi
+
+    sleep 5
+  done
+}
+
+cleanup() {
+  wait $SIDE_CAR_PID &&
+    if [[ -f $TERMINATED_FILE ]]; then
+      exit 0
+    fi
+  }
+
+launch_main_container &
+SIDE_CAR_PID=$! && watch_for_termination &
+cleanup
+
